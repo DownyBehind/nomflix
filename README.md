9113bb8edbb51cd73957128f1f636775b364c593
downy <TeamBehindDowny@gmail.com>
2019-12-01 18:13:50 +0900

    chapter3 finish

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ Learning React and ES6 by building a Movie Doscovery App.
 - [ ] TV Shows
 - [ ] Search Route
 - [ ] Detail
+
+## API Verbs
+
+- [ ] Now playing (Movie)
+- [ ] Upcoming (Movie)
+- [ ] Top Rated (TV)
+- [ ] Popular (TV, Moive)
+- [ ] Airing Today (TV)
+- [ ] TV Show Detail
+- [ ] Movie Detail
+- [ ] Search (Movie, TV)
---
+++ src/api.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: "abf20b7d47d7a88c31588ccc5b3d1c72",
+    language: "en-US"
+  }
+});
+
+export const moviesApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular"),
+  movieDetail: id =>
+    api.get(`movie/${id}`, {
+      params: {
+        append_toresponse: "vidoes"
+      }
+    }),
+  search: term =>
+    api.get("search/movie", {
+      params: {
+        query: encodeURIComponent(term)
+      }
+    })
+};
+
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+  showDetail: id =>
+    api.get(`tv/${id}`, {
+      params: {
+        append_toresponse: "vidoes"
+      }
+    }),
+  search: term =>
+    api.get("search/tv", {
+      params: {
+        query: encodeURIComponent(term)
+      }
+    })
+};
